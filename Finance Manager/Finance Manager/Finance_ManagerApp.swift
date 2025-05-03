3237a3be0119de959b273ff2fa8ba7061cb4be2e
Kartik Patel <kartik.patel@openlane.com>
2025-05-03 14:04:43 -0400

    init ios app

---
+++ Finance Manager/Finance Manager/Finance_ManagerApp.swift
@@ -0,0 +1,32 @@
+//
+//  Finance_ManagerApp.swift
+//  Finance Manager
+//
+//  Created by Kartik Patel on 2025-05-03.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Finance_ManagerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
